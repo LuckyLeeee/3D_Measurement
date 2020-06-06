1e378ae9768c5894774da8edb21291b6f2819b01
LuckyLeeee <60919627+LuckyLeeee@users.noreply.github.com>
2020-06-06 20:10:49 +0700

    Add files via upload

---
+++ 3D_Measurement/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace _3D_Measurement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.import = new System.Windows.Forms.Button();
+            this.cd = new System.Windows.Forms.TextBox();
+            this.c7 = new System.Windows.Forms.TextBox();
+            this.cv = new System.Windows.Forms.TextBox();
+            this.ce = new System.Windows.Forms.TextBox();
+            this.cb = new System.Windows.Forms.TextBox();
+            this.cg = new System.Windows.Forms.TextBox();
+            this.cm = new System.Windows.Forms.TextBox();
+            this.chieucao = new System.Windows.Forms.TextBox();
+            this.chieu7 = new System.Windows.Forms.TextBox();
+            this.chieuvu = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // import
+            // 
+            this.import.Location = new System.Drawing.Point(12, 31);
+            this.import.Name = "import";
+            this.import.Size = new System.Drawing.Size(102, 33);
+            this.import.TabIndex = 0;
+            this.import.Text = "Import";
+            this.import.UseVisualStyleBackColor = true;
+            this.import.Click += new System.EventHandler(this.import_Click);
+            // 
+            // cd
+            // 
+            this.cd.Location = new System.Drawing.Point(342, 31);
+            this.cd.Name = "cd";
+            this.cd.ReadOnly = true;
+            this.cd.Size = new System.Drawing.Size(100, 22);
+            this.cd.TabIndex = 1;
+            // 
+            // c7
+            // 
+            this.c7.Location = new System.Drawing.Point(342, 72);
+            this.c7.Name = "c7";
+            this.c7.ReadOnly = true;
+            this.c7.Size = new System.Drawing.Size(100, 22);
+            this.c7.TabIndex = 2;
+            // 
+            // cv
+            // 
+            this.cv.Location = new System.Drawing.Point(342, 120);
+            this.cv.Name = "cv";
+            this.cv.ReadOnly = true;
+            this.cv.Size = new System.Drawing.Size(100, 22);
+            this.cv.TabIndex = 3;
+            // 
+            // ce
+            // 
+            this.ce.Location = new System.Drawing.Point(342, 165);
+            this.ce.Name = "ce";
+            this.ce.ReadOnly = true;
+            this.ce.Size = new System.Drawing.Size(100, 22);
+            this.ce.TabIndex = 4;
+            // 
+            // cb
+            // 
+            this.cb.Location = new System.Drawing.Point(342, 207);
+            this.cb.Name = "cb";
+            this.cb.ReadOnly = true;
+            this.cb.Size = new System.Drawing.Size(100, 22);
+            this.cb.TabIndex = 5;
+            // 
+            // cg
+            // 
+            this.cg.Location = new System.Drawing.Point(342, 251);
+            this.cg.Name = "cg";
+            this.cg.ReadOnly = true;
+            this.cg.Size = new System.Drawing.Size(100, 22);
+            this.cg.TabIndex = 6;
+            // 
+            // cm
+            // 
+            this.cm.Location = new System.Drawing.Point(342, 302);
+            this.cm.Name = "cm";
+            this.cm.ReadOnly = true;
+            this.cm.Size = new System.Drawing.Size(100, 22);
+            this.cm.TabIndex = 7;
+            // 
+            // chieucao
+            // 
+            this.chieucao.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chieucao.Location = new System.Drawing.Point(149, 31);
+            this.chieucao.Name = "chieucao";
+            this.chieucao.ReadOnly = true;
+            this.chieucao.Size = new System.Drawing.Size(169, 22);
+            this.chieucao.TabIndex = 8;
+            this.chieucao.Text = "Chiều cao";
+            // 
+            // chieu7
+            // 
+            this.chieu7.Location = new System.Drawing.Point(149, 72);
+            this.chieu7.Name = "chieu7";
+            this.chieu7.ReadOnly = true;
+            this.chieu7.Size = new System.Drawing.Size(169, 22);
+            this.chieu7.TabIndex = 9;
+            this.chieu7.Text = "Chiều cao đốt sống cổ số 7";
+            // 
+            // chieuvu
+            // 
+            this.chieuvu.Location = new System.Drawing.Point(149, 120);
+            this.chieuvu.Name = "chieuvu";
+            this.chieuvu.ReadOnly = true;
+            this.chieuvu.Size = new System.Drawing.Size(169, 22);
+            this.chieuvu.TabIndex = 10;
+            this.chieuvu.Text = "Chiều cao đầu vú";
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(149, 165);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.ReadOnly = true;
+            this.textBox11.Size = new System.Drawing.Size(169, 22);
+            this.textBox11.TabIndex = 11;
+            this.textBox11.Text = "Chiều cao eo";
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(149, 207);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.ReadOnly = true;
+            this.textBox12.Size = new System.Drawing.Size(169, 22);
+            this.textBox12.TabIndex = 12;
+            this.textBox12.Text = "Chiều cao bụng";
+            // 
+            // textBox13
+            // 
+            this.textBox13.Location = new System.Drawing.Point(149, 251);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.ReadOnly = true;
+            this.textBox13.Size = new System.Drawing.Size(169, 22);
+            this.textBox13.TabIndex = 13;
+            this.textBox13.Text = "Chiều cao đầu gối";
+            // 
+            // textBox14
+            // 
+            this.textBox14.Location = new System.Drawing.Point(149, 302);
+            this.textBox14.Name = "textBox14";
+            this.textBox14.ReadOnly = true;
+            this.textBox14.Size = new System.Drawing.Size(169, 22);
+            this.textBox14.TabIndex = 14;
+            this.textBox14.Text = "Chiều cao mông";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 359);
+            this.Controls.Add(this.textBox14);
+            this.Controls.Add(this.textBox13);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.chieuvu);
+            this.Controls.Add(this.chieu7);
+            this.Controls.Add(this.chieucao);
+            this.Controls.Add(this.cm);
+            this.Controls.Add(this.cg);
+            this.Controls.Add(this.cb);
+            this.Controls.Add(this.ce);
+            this.Controls.Add(this.cv);
+            this.Controls.Add(this.c7);
+            this.Controls.Add(this.cd);
+            this.Controls.Add(this.import);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button import;
+        private System.Windows.Forms.TextBox cd;
+        private System.Windows.Forms.TextBox c7;
+        private System.Windows.Forms.TextBox cv;
+        private System.Windows.Forms.TextBox ce;
+        private System.Windows.Forms.TextBox cb;
+        private System.Windows.Forms.TextBox cg;
+        private System.Windows.Forms.TextBox cm;
+        private System.Windows.Forms.TextBox chieucao;
+        private System.Windows.Forms.TextBox chieu7;
+        private System.Windows.Forms.TextBox chieuvu;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.TextBox textBox14;
+    }
+}
+
---
+++ 3D_Measurement/Form1.cs
@@ -0,0 +1,72 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3D_Measurement
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void import_Click(object sender, EventArgs e)
+        {
+            string fileName;
+            using(OpenFileDialog openFileDialog = new OpenFileDialog() { Filter= "Objects File |*.obj" })
+            {
+                if(openFileDialog.ShowDialog()==DialogResult.OK)
+                {
+                    fileName = Path.GetFileName(openFileDialog.FileName);
+                    ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+                    var package = new ExcelPackage(new FileInfo("FemaleBodyDataSet.xlsx"));
+                    ExcelWorksheet workSheet = package.Workbook.Worksheets[0];
+                    for (int i = workSheet.Dimension.Start.Row + 1; i <= workSheet.Dimension.End.Row; i++)
+                    {
+                        if (fileName.StartsWith(workSheet.Cells[i, 1].Value.ToString()))
+                        {
+                            cd.Text = workSheet.Cells[i, 2].Value.ToString();
+                            c7.Text = workSheet.Cells[i, 3].Value.ToString();
+                            cv.Text = workSheet.Cells[i, 4].Value.ToString();
+                            ce.Text = workSheet.Cells[i, 5].Value.ToString();
+                            cb.Text = workSheet.Cells[i, 6].Value.ToString();
+                            cg.Text = workSheet.Cells[i, 7].Value.ToString();
+                            cm.Text = workSheet.Cells[i, 8].Value.ToString();
+                        }
+                    }
+                }  
+            }    
+            try
+            {
+                var package = new ExcelPackage(new FileInfo("FemaleBodyDataSet.xlsx"));
+
+                ExcelWorksheet workSheet = package.Workbook.Worksheets[1];
+
+                for (int i = workSheet.Dimension.Start.Row + 1; i <= workSheet.Dimension.End.Row; i++)
+                {
+                    try
+                    {
+                      //cd.Text = workSheet.Cells[1, 1].Value.ToString();
+                    }
+                    catch
+                    {
+
+                    }
+                } 
+            }
+            catch
+            {
+
+            }
+        }
+
+    }
+}
